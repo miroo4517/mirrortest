edfe21038b7e5605b166e79978f3fc30a7c3c6f4
MiRoo <miroo4517@gmail.com>
2021-01-25 23:47:38 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/*Copyright (c) 2021 by Ben Matthews (https://codepen.io/tsuhre/pen/BYbjyg)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
 let points = [];
 let tickSpeed = 10;
 let base = 180;
@@ -11,7 +19,6 @@ function Point(x = random(width), y = random(height), a = random(PI)){
   this.a = a;
   this.dx = cos(a);
   this.dy = sin(a);
-  this.hue = (random(100) + base)%360;
   this.color = color(214, 89.3, 52.2, .01);
 }
 
